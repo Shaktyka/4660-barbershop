973635b1fbfa50626aa8d06cb594a5e1dd4ead31
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-08-25 11:14:49 +0600

    Добавляет открытие-закрытие окна входа

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-const loginBtn = document.querySelector(`.login-link`);
-const loginWindow = document.querySelector(`.modal-login`);
+const loginLink = document.querySelector(`.login-link`);
+const loginPopup = document.querySelector(`.modal-login`);
+const loginClose = loginPopup.querySelector(`.modal-close`);
+const loginSubmit = loginPopup.querySelector(`.login-submit`);
 
-const loginBtnClickHandler = (evt) => {
+const loginForm = loginPopup.querySelector(`.login-form`);
+
+const userLoginField = loginForm.querySelector(`#user-login`);
+const userPassField = loginForm.querySelector(`#user-password`);
+const rememberCheck = loginForm.querySelector(`.login-checkbox input`);
+const restoreLink = loginForm.querySelector(`.restore`);
+
+// const loginSubmitClickHandler = (evt) => {
+//   evt.preventDefault();
+//   loginPopup.classList.remove(`modal-show`);
+// };
+
+const loginCloseClickHandler = (evt) => {
   evt.preventDefault();
-  console.log(1);
+  loginPopup.classList.remove(`modal-show`);
 };
 
-loginBtn.addEventListener(`click`, loginBtnClickHandler);
+const loginLinkClickHandler = (evt) => {
+  evt.preventDefault();
+  loginPopup.classList.add(`modal-show`);
+  userLoginField.focus();
+};
+
+const loginFormSubmitHandler = (evt) => {
+  evt.preventDefault();
+};
+
+loginForm.addEventListener(`submit`, loginFormSubmitHandler);
+// loginSubmit.addEventListener(`click`, loginSubmitClickHandler);
+loginClose.addEventListener(`click`, loginCloseClickHandler);
+loginLink.addEventListener(`click`, loginLinkClickHandler);
 
