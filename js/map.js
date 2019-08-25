e1d11f876e4c4e148b507104b29365ffde449482
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-08-25 16:39:21 +0600

    Добавляет открытие\закрытие окна с картой

---
+++ js/map.js
@@ -0,0 +1,27 @@
+const mapLink = document.querySelector(`.contacts-button-map`);
+
+const mapPopup = document.querySelector(`.modal-map`);
+const mapClose = mapPopup.querySelector(`.modal-close`);
+
+const mapLinkClickHandler = (evt) => {
+  evt.preventDefault();
+  mapPopup.classList.add(`modal-show`);
+};
+
+const mapCloseClickHandler = (evt) => {
+  evt.preventDefault();
+  mapPopup.classList.remove(`modal-show`);
+};
+
+const windowKeydownHandler = (evt) => {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains(`modal-show`)) {
+      mapPopup.classList.remove(`modal-show`);
+    }
+  }
+};
+
+window.addEventListener(`keydown`, windowKeydownHandler);
+mapClose.addEventListener(`click`, mapCloseClickHandler);
+mapLink.addEventListener(`click`, mapLinkClickHandler);
