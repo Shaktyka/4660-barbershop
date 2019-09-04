81960b258e4c8352558f048e9aceeb3481301459
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-09-04 09:38:39 +0500

    Добавлены изображения для слайдера

---
+++ js/slider.js
@@ -0,0 +1,32 @@
+const gallery = document.querySelector(`.gallery-container`);
+const backBtn = gallery.querySelector(`.gallery-button-back`);
+const nextBtn = gallery.querySelector(`.gallery-button-next`);
+const photo = gallery.querySelector(`gallery-foto`);
+
+const PHOTOS = [
+  {
+  	title: ``,
+  	img: ``,
+  	href: #
+  },
+  {
+  	title: ``,
+  	img: ``,
+  	href: #
+  },
+  {
+  	title: ``,
+  	img: ``,
+  	href: #
+  },
+  {
+  	title: ``,
+  	img: ``,
+  	href: #
+  },
+  {
+  	title: ``,
+  	img: ``,
+  	href: #
+  }
+];
