b1e24cdc173f2ab8f57030b8152dfe64b1158ef2
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-09-06 00:29:20 +0600

    Добавляет рендеринг данных продукта на странице описания

--- js/product.js
+++ js/product.js
@@ -8,48 +8,37 @@ const createElement = (string) => {
   return div.firstChild;
 };
 
-const getProductPhoto = ({id, category, name}) => {
-  console.log(id);
-  return `<section class="product-photos">
-        <h2 class="visually-hidden">Изображения товара</h2>
-        <p class="product-photo-full">
-          <img src="img/item-1-big.jpg" width="460" height="498" alt="${category} «${name}»">
-        </p>
-        <ul class="product-photo-preview">
-          <li>
-            <img src="img/item-1.jpg" width="140" height="149" alt="Набор для путешествий «Baxter of California»">
-          </li>
-          <li>
-            <img src="img/item-2.jpg" width="140" height="149" alt="Набор для путешествий «Baxter of California»">
-          </li>
-          <li>
-            <img src="img/item-3.jpg" width="140" height="149" alt="Набор для путешествий «Baxter of California»">
-          </li>
-        </ul>
-      </section>`.trim();
+const getProductPhoto = ({id, category, name, bigPicture, previews}) => {
+  return `<section class="product-photos" data-id="${id}">
+    <h2 class="visually-hidden">Изображения товара</h2>
+    <p class="product-photo-full">
+      <img src="img/${bigPicture}" width="460" height="498" alt="${category} «${name}»">
+    </p>
+    <ul class="product-photo-preview">
+      ${Array.from(previews).map((preview) => `<li>
+        <img src="img/${preview}" width="140" height="149" alt="${category} «${name}»">
+      </li>`).join(``)}
+    </ul>
+  </section>`.trim();
 };
 
-const getProductInfo = ({id}) => {
-  return `<section class="product-info">
-        <h2 class="visually-hidden">Описание товара</h2>
-        <div class="product-description">
-          <p class="product-availability">Есть в наличии</p>
-          <p class="product-articul">Articul: dexter-ae</p>
-        </div>
-        <p class="product-text">Travel Kit – необходимый аксессуар во время любого путешествия. В аккуратной кожаной сумке находится все, что нужно для бритья и ухода за кожей во время рабочей поездки или отдыха: средство для умывания, увлажняющий крем, крем для бритья, крем после бритья, шампунь. Набор также может стать отличным подарком.</p>
-        <p class="product-price">
-          <b>2900 ₽</b>
-          <a class="button" href="#">Купить</a>
-        </p>
-        <h3>В набор входят:</h3>
-        <ul class="product-info">
-          <li>Средство для умывания (50 мл)</li>
-          <li>Увлажняющий крем (50 мл)</li>
-          <li>Крем для бритья (50 мл)</li>
-          <li>Крем после бритья, шампунь (50 мл)</li>
-          <li>Удобная кожаная косметичка</li>
-        </ul>
-      </section>`.trim();
+const getProductInfo = ({id, inStock, articul, text, productInfo, price}) => {
+  return `<section class="product-info" data-id="${id}">
+    <h2 class="visually-hidden">Описание товара</h2>
+    <div class="product-description">
+      <p class="product-availability">${inStock ? `Есть в наличии` : `Нет в наличии`}</p>
+      <p class="product-articul">Articul: ${articul}</p>
+    </div>
+    <p class="product-text">${text}</p>
+    <p class="product-price">
+      <b>${price} ₽</b>
+      <a class="button" href="#">Купить</a>
+    </p>
+    <h3>В набор входят:</h3>
+    <ul class="product-info">
+      ${Array.from(productInfo).map((item) => `<li>${item}</li>`).join(``)}
+    </ul>
+  </section>`.trim();
 };
 
 // Получаем id товара из параметров запроса
